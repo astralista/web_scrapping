3711a250e6d1a825aecafc018acedea8c58c0bfd
Godz Dmitriy <woke@mail.ru>
2023-03-23 14:44:49 +0300

    all done __except USD task

--- main.py
+++ main.py
@@ -1,51 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
-
+import json
+import csv
 
 url = 'https://hh.ru/search/vacancy?area=1&area=2&text=python'
 
 def get_headers():
     return Headers(browser="firefox", os="win").generate()
 
+# Идем на страницу поиска и выбираем блоки с описанием вакансии:
 page = requests.get(url, headers=get_headers())
 text = page.text
 soup = BeautifulSoup(text, features='lxml')
-vacancies = soup.find_all(class_="serp-item")
+vacancies = []
 
+# ключевики для поиска по критерию:
 keywords = ['Django', 'Flask']
 
-for vacancy in vacancies:
+# обходим блоки с вакансиями для фильтрации:
+for vacancy in soup.find_all(class_="serp-item"):
     # смотрим страницу со всеми вакансиями:
     vacancy_name = vacancy.find('h3').find('a')
     vacancy_url = vacancy_name.attrs['href']
-    company_name = vacancy.find(class_="vacancy-serp-item__meta-info-company").find('a')
-    city = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy-address'})
     salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
-    if salary is not None:
-        print(salary.text)
-    else:
-        print("Данные о зарплате не найдены")
+    company_name = vacancy.find(class_="vacancy-serp-item__meta-info-company").find('a').text
+    city = vacancy.find('div', {'data-qa': 'vacancy-serp__vacancy-address'}).text
 
+    # идем на страницу вакансии:
+    page_vacancy = requests.get(vacancy_url, headers=get_headers())
+    vac_text = page_vacancy.text
+    vacancy_soup = BeautifulSoup(vac_text, 'lxml')
 
-    # Проверяем условия поиска:
-    # for data in vacancy:
-    #     page = requests.get(vacancy_url, headers=get_headers())
-    #     text = page.text
-    #     vacancy_soup = BeautifulSoup(text, 'html.parser')
-    #     skill = soup.find(class_="bloko-tag-list")
-    #     for i in skill:
-    #         skill_name = skill.find('span')
-    #         print(skill_name.text)
+    vacancy_info = {}
+    # применяем ключи для фильтрации:
+    if all(keyword.lower() in vacancy_soup.text.lower() for keyword in keywords):
+        vacancy_info['link'] = vacancy_url
+        vacancy_info['salary'] = salary.text
+        vacancy_info['company'] = company_name
+        vacancy_info['city'] = city
+        vacancies.append(vacancy_info)
 
+with open('vacancies.json', 'w', encoding='utf-8') as f:
+    json.dump(vacancies, f, ensure_ascii=False, indent=4)
 
-
-    # поиск на другой странице
-    # descr = (BeautifulSoup(requests.get(links, headers=headers).text, features='lxml')).find('div', {
-    #     'data-qa': 'vacancy-description'}).text
-
-    # print(vacancy_name.text, '-->', url)
-
+with open('vacancies.csv', 'w', encoding='utf-8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Link', 'Salary', 'Company', 'City'])
+    for vacancy in vacancies:
+        writer.writerow([vacancy['link'], vacancy['salary'], vacancy['company'], vacancy['city']])
 
 if '__name__' == '__main__':
     main()
